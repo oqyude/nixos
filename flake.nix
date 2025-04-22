82f4e7083c528851dc04af03f72aeeef0a58cc9b
oqyude@gmail.com <oqyude>
2025-04-22 13:21:44 +0300

    123

--- flake.nix
+++ flake.nix
@@ -3,11 +3,12 @@
 
   inputs = {
 
-    nixpkgs.url = "flake:nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/f6db44a8daa59c40ae41ba6e5823ec77fe0d2124";
 
-    nixpkgs-unstable.url = "flake:nixpkgs/nixpkgs-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; #"flake:nixpkgs/nixpkgs-unstable"
 
-    # last working commit - "github:NixOS/nixpkgs/f6db44a8daa59c40ae41ba6e5823ec77fe0d2124";
+    flake-compat.url = "github:edolstra/flake-compat";
+
     home-manager = {
       url = "flake:home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -22,7 +23,10 @@
     };
     aagl = {
       url = "github:ezKEa/aagl-gtk-on-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-compat.follows = "flake-compat";
+      };
     };
 
   };
@@ -31,8 +35,8 @@
     inputs:
     let
       zeroq = import ./modules/vars.nix;
-      pkgs-unstable = import inputs.nixpkgs-unstable { system = "x86_64-linux"; };
 
+      pkgs-unstable = import inputs.nixpkgs-unstable { system = "x86_64-linux"; };
       flakeContext = { inherit inputs zeroq pkgs-unstable; };
     in
 
