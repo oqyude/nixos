e66f7625daf005e45c74c2f8b2e96224bed855b2
oqyude@gmail.com <oqyude>
2025-04-24 20:51:22 +0300

    123

--- modules/special/atoridu.nix
+++ modules/special/atoridu.nix
@@ -8,8 +8,13 @@
 {
   imports = [
     ../hardware/fingerprint.nix
+    ../audio/preempt_rt.nix
 
     inputs.self.nixosModules.additional.musnix # musnix module
     inputs.self.nixosModules.additional.aagl # aagl module
   ];
+
+#   rtnix.kernel.realtime.enable = true;
+#   rtnix.tuningProcesses = [ "irq/.*xhci" "irq/.*snd_intel_hda" ];
+
 }
