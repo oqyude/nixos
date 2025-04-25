f772f8bd0e77f933a6da380fc5172e0d2a2f7114
oqyude@gmail.com <oqyude>
2025-04-26 00:11:32 +0300

    123

--- modules/hardware/fingerprint.nix
+++ modules/hardware/fingerprint.nix
@@ -19,32 +19,28 @@
       #tod.driver = pkgs.libfprint-2-tod1-goodix-550a;
     };
   };
-#
-#   environment.systemPackages = with pkgs; [
-#     fprintd
-#     open-fprintd
+
+
+
+#   nixpkgs.overlays = [
+#     (final: prev: {
+#       libfprint = prev.libfprint.overrideAttrs (oldAttrs: {
+#         version = "git";
+#         src = final.fetchFromGitHub {
+#           owner = "ericlinagora";
+#           repo = "libfprint-CS9711";
+#           rev = "03ace5b20146eb01c77fb3ea63e1909984d6d377";
+#           sha256 = "sha256-gr3UvFB6D04he/9zawvQIuwfv0B7fEZb6BGiNAbLids=";
+#         };
+#         nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
+#           final.opencv
+#           final.cmake
+#           final.doctest
+#           final.nss
+#         ];
+#       });
+#     })
 #   ];
-
-
-  nixpkgs.overlays = [
-    (final: prev: {
-      libfprint = prev.libfprint.overrideAttrs (oldAttrs: {
-        version = "git";
-        src = final.fetchFromGitHub {
-          owner = "ericlinagora";
-          repo = "libfprint-CS9711";
-          rev = "c242a40fcc51aec5b57d877bdf3edfe8cb4883fd";
-          sha256 = "sha256-WFq8sNitwhOOS3eO8V35EMs+FA73pbILRP0JoW/UR80=";
-        };
-        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
-          final.opencv
-          final.cmake
-          final.doctest
-          final.nss
-        ];
-      });
-    })
-  ];
 
 
 }
