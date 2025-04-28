77430517a28ffa0a3064eaf2adc7d94c8d12bd51
oqyude@gmail.com <oqyude>
2025-04-28 21:57:49 +0300

    123

--- modules/software/zapret.nix
+++ modules/software/zapret.nix
@@ -2,26 +2,25 @@
   config,
   lib,
   pkgs,
-  inputs,
   ...
 }:
 {
-  services.zapret = {
-    enable = true;
-    httpMode = "full";
-    udpSupport = true;
-    udpPorts = [
-      "50000:65535"
-    ];
-    params = [
-      "--dpi-desync=fake,tamper"
-      "--dpi-desync-repeats=6"
-      "--dpi-desync-any-protocol"
-    ];
-  };
-  #   users.users.${inputs.zeroq.devices.admin} = {
+  #   services.zapret = {
+  #     enable = true;
+  #     httpMode = "full";
+  #     udpSupport = true;
+  #     udpPorts = [
+  #       "50000:65535"
+  #     ];
+  #     params = [
+  #       "--dpi-desync=fake,tamper"
+  #       "--dpi-desync-repeats=6"
+  #       "--dpi-desync-any-protocol"
+  #     ];
+  #   };
+  #   users.users.oqyude = {
   #     packages = with pkgs; [
   #       zapret
   #     ];
-  #   };
+  #   };\
 }
