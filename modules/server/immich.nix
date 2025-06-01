0fcaf6156affc16e7faf98441f4b78f0ef48f1c4
oqyude <oqyude@gmail.com>
2025-06-01 17:48:01 +0300

    123

--- modules/server/immich.nix
+++ modules/server/immich.nix
@@ -17,8 +17,22 @@
       openFirewall = true;
       accelerationDevices = null;
       machine-learning.enable = false;
-      mediaLocation = "${inputs.zeroq.dirs.immich-folder}";
+      #mediaLocation = "${inputs.zeroq.dirs.immich-folder}";
     };
+  };
+
+  # immich
+  fileSystems."${services.immich.mediaLocation}" = {
+    device = "${inputs.zeroq.dirs.immich-folder}";
+    options = [
+      "bind"
+      # "uid=1000"
+      # "gid=1000"
+      # "fmask=0007"
+      # "dmask=0007"
+      "nofail"
+      "x-systemd.device-timeout=0"
+    ];
   };
 
   users.users.immich.extraGroups = [
