73206ccc37356e520816d888757f48f988b5f9f0
oqyude <oqyude@gmail.com>
2025-06-05 21:43:28 +0300

    123

--- modules/server/cloudflared.nix
+++ modules/server/cloudflared.nix
@@ -13,13 +13,23 @@
       certificateFile = "${inputs.zeroq.dirs.server-home}/Credentials/server/cloudflared/cert.pem";
       tunnels = {
         "e5d66ea5-d6d2-4eef-9b34-82696946ef58" = {
-          credentialsFile = "${inputs.zeroq.dirs.server-home}/Credentials/server/cloudflared/immich.json";
+          credentialsFile = "${inputs.zeroq.dirs.server-credentials}/cloudflared/immich.json";
+          warp-routing.enabled = false;
+          originRequest = {
+            tlsTimeout = "15s";
+            tcpKeepAlive = "30s";
+            noHappyEyeballs = false;
+            keepAliveTimeout = "1m30s";
+            connectTimeout = "1m";
+          };
           ingress = {
             "immich.zeroq.ru" = {
               service = "http://localhost:2283";
             };
+            "nextcloud.zeroq.ru" = {
+              service = "http://localhost:10000";
+            };
           };
-          warp-routing.enabled = true;
           default = "http_status:404";
         };
       };
