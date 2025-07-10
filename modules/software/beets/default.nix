73f23c450b72fe3ce16470d63fb760e159463b2e
oqyude <oqyude@gmail.com>
2025-07-10 15:39:07 +0300

    123

--- modules/software/beets/default.nix
+++ modules/software/beets/default.nix
@@ -14,7 +14,7 @@ let
   python3 = pkgs.python3.override {
     packageOverrides = depsOverlay;
   };
-  beetsEnv = python3.withPackages (ps: [ ps.beets ]);
+  beetsEnv = python3.withPackages (ps: [ ps.beets /*ps.gst-python ps.pygobject3*/ ]);
 in
 {
   systemd.tmpfiles.rules = [
@@ -26,6 +26,7 @@ in
       "${inputs.zeroq.devices.admin}" = {
         packages = [
           beetsEnv
+          pkgs.mp3gain
           pkgs.imagemagick
           pkgs.ffmpeg
         ];
