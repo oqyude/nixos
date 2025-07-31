9f2ba38940e9e1e31b28270f822f6bba7771b38b
oqyude <oqyude@gmail.com>
2025-07-31 15:28:38 +0300

    123

--- modules/essentials/settings.nix
+++ modules/essentials/settings.nix
@@ -10,6 +10,10 @@
       substituters = [
         "https://nixos-cache-proxy.cofob.dev" # https://gist.github.com/cofob/9b1fd205e6d961a45c225ae9f0af1394
         "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       auto-optimise-store = true;
       experimental-features = [
