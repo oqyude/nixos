32f2f07307e4bc48d85e42431c5c93dc4b80861f
oqyude <oqyude@gmail.com>
2025-08-01 13:12:54 +0300

    123

--- modules/server/ai.nix
+++ modules/server/ai.nix
@@ -6,7 +6,7 @@
   ...
 }:
 let
-  unfree = import inputs.nixpkgs-webui {
+  fix = import inputs.nixos {
     system = "x86_64-linux";
     config.allowUnfree = true;
   }; # temp
@@ -15,7 +15,7 @@ in
   services = {
     open-webui = {
       enable = true;
-      #package = unfree.open-webui;
+      package = fix.open-webui;
       host = "0.0.0.0";
       port = 11111;
       openFirewall = true;
@@ -25,7 +25,10 @@ in
         SCARF_NO_ANALYTICS = "True";
         OPENAI_API_BASE_URL = "http://localhost:1234/v1";
         #OLLAMA_API_BASE_URL = "http://127.0.0.1:1234";
-        #WEBUI_AUTH = "False"; # Disable authentication
+        WEBUI_AUTH = "True"; # Disable authentication
+        ENABLE_SIGNUP = "True";
+        ENABLE_SIGNUP_PASSWORD_CONFIRMATION = "True";
+        ENABLE_VERSION_UPDATE_CHECK = "False";
       };
     };
   };
