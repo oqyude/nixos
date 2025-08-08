4f730c25ea493565f8ec0e32cb80c3bf38a355b8
oqyude <oqyude@gmail.com>
2025-08-08 22:18:07 +0300

    123

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -1,6 +1,43 @@
 { ... }@flakeContext:
+let
+defaultModule =
+{
+  config,
+  lib,
+  xlib,
+  deviceType,
+  ...
+}:
+{
+  imports = with inputs; [
+    ./essentials
+    ./users.nix
+    ./options.nix
+    ./temp.nix
+    (./. + "/${deviceType}") # specific modules
+
+    home-manager.nixosModules.home-manager # home-manager module
+    nix-index-database.nixosModules.nix-index # nix-index module
+    grub2-themes.nixosModules.default # grub2 themes module
+    sops-nix.nixosModules.sops # sops module
+    self.homeConfigurations.default.nixosModule # default homeConfigurations
+    disko.nixosModules.disko # disko module
+  ];
+
+  home-manager = {
+    extraSpecialArgs = {
+      xlib = config.xlib;
+    };
+  };
+
+  _module.args = {
+    inputs = inputs;
+    xlib = config.xlib;
+  };
+}
+in
 {
   nixosModules = {
-    default = import ./base.nix flakeContext;
+    default = defaultModule; #import ./base.nix flakeContext;
   };
 }
