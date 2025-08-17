0c698859e3fb2aa5a34fb060278e29a08873e3c7
oqyude <oqyude@gmail.com>
2025-08-17 15:19:13 +0300

    123

--- nixosModules/vds/nginx.nix
+++ nixosModules/vds/nginx.nix
@@ -16,23 +16,31 @@ in
       virtualHosts = {
         "immich.zeroq.ru" = {
           # 31.57.105.253
-          forceSSL = true;
+          addSSL = true;
           enableACME = true;
           locations."/" = {
             proxyPass = "http://${server}:2283"; # Порт Immich
             proxyWebsockets = true; # Если Immich использует WebSockets
           };
+          locations."/.well-known/acme-challenge/" = {
+            root = "/var/lib/acme/acme-challenge";
+            tryFiles = "$uri $uri/ =404";
+          };
         };
         "nextcloud.zeroq.ru" = {
-          forceSSL = true;
+          addSSL = true;
           enableACME = true;
           locations."/" = {
             proxyPass = "http://${server}:10000"; # Порт Nextcloud
             proxyWebsockets = true;
           };
+          locations."/.well-known/acme-challenge/" = {
+            root = "/var/lib/acme/acme-challenge";
+            tryFiles = "$uri $uri/ =404";
+          };
         };
         # "llm.zeroq.ru" = {
-        #   forceSSL = true;
+        #   addSSL = true;
         #   enableACME = true;
         #   locations."/" = {
         #     proxyPass = "http://atoridu.laxta-platy.ts.net:11111"; # Порт Open WebUI
@@ -47,8 +55,6 @@ in
     defaults.email = "oqyude@gmail.com";
   };
   networking.firewall.allowedTCPPorts = [
-    # 2283
-    # 10000
     80
     443
   ];
