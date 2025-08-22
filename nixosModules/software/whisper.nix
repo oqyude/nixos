27261809bf278a173ffe887b328458f7aa3555b2
oqyude <oqyude@gmail.com>
2025-08-22 18:51:08 +0300

    123

--- nixosModules/software/whisper.nix
+++ nixosModules/software/whisper.nix
@@ -9,4 +9,8 @@
   environment.systemPackages = [
     pkgs.openai-whisper
   ];
+  nixpkgs.config.rocmSupport = true;
+  nixpkgs.config.cudaSupport = false;
+  hardware.amdgpu.opencl.enable = true;
+  hardware.graphics.extraPackages = with pkgs.rocmPackages; [ clr clr.icd ];
 }
