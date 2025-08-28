e9205cfc37f99e0a71f463fdc7919d5e19dabc6c
oqyude <oqyude@gmail.com>
2025-08-28 14:17:35 +0300

    flake update

--- homeConfigurations/example of symlinking.nix
+++ homeConfigurations/example of symlinking.nix
@@ -1,23 +1,28 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   dotfiles = "${config.home.homeDirectory}/dotfiles/.configs";
 
   symlinks = {
-    "hypr"              = ".config/hypr";
-    "waybar"            = ".config/waybar";
-    "rofi"              = ".config/rofi";
-    "yazi"              = ".config/yazi";
+    "hypr" = ".config/hypr";
+    "waybar" = ".config/waybar";
+    "rofi" = ".config/rofi";
+    "yazi" = ".config/yazi";
 
-    "alacritty"         = ".config/alacritty";
-    "zellij"            = ".config/zellij";
+    "alacritty" = ".config/alacritty";
+    "zellij" = ".config/zellij";
 
-    "zed"               = ".config/zed";
+    "zed" = ".config/zed";
 
-    "lazygit"           = ".config/lazygit";
+    "lazygit" = ".config/lazygit";
 
-    "fish/config.fish"  = ".config/fish/config.fish";
-    "git/.gitconfig"    = ".gitconfig";
-    "npm/.npmrc"        = ".npmrc";
+    "fish/config.fish" = ".config/fish/config.fish";
+    "git/.gitconfig" = ".gitconfig";
+    "npm/.npmrc" = ".npmrc";
   };
 
   mkLinks = lib.mapAttrs' (src: target: {
@@ -25,7 +30,8 @@ let
     value.source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/${src}";
   }) symlinks;
 
-in {
+in
+{
   home.username = "tedna";
   home.homeDirectory = "/home/tedna";
   home.stateVersion = "25.11";
