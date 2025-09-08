ad33b3cc071e2667aa632504012a803ec51e9bd1
oqyude <oqyude@gmail.com>
2025-09-08 15:07:19 +0300

    123

---
+++ nixosModules/software/aichat.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  lib,
+  pkgs,
+  xlib,
+  ...
+}:
+{
+  # services = {
+  #   nextjs-ollama-llm-ui.enable = false;
+  #   ollama = {
+  #     enable = false;
+  #     package = pkgs.ollama-rocm;
+  #     environmentVariables = {
+  #       HSA_OVERRIDE_GFX_VERSION = "11.5.0";
+  #       HCC_AMDGPU_TARGET = "gfx1150"; # used to be necessary, but doesn't seem to anymore
+  #     };
+  #     user = "ollama"; # "${xlib.device.username}";
+  #     group = "ollama";
+  #     acceleration = "rocm";
+  #     rocmOverrideGfx = "11.5.0";
+  #   };
+  # };
+}
--- nixosModules/wsl/default.nix
+++ nixosModules/wsl/default.nix
@@ -7,6 +7,7 @@
   imports = [
     ../software/beets
     ../software/whisper.nix
+    ../software/aichat.nix
     #../services/tts.nix
     #../server/open-webui.nix
   ];
