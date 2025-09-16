9146debc1ddb278586dcc1587bd902e10a322995
oqyude <oqyude@gmail.com>
2025-09-16 18:45:28 +0300

    123

--- homeConfigurations/modules/external.nix
+++ homeConfigurations/modules/external.nix
@@ -2,13 +2,14 @@
   config,
   pkgs,
   inputs,
+  xlib,
   ...
 }:
 {
   home.file = {
-      "Storage" = {
-        source = config.lib.file.mkOutOfStoreSymlink "${xlib.dirs.vetymae-drive}/Users/oqyude/Storage";
-        target = "Storage";
-      };
+    "Storage" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${xlib.dirs.vetymae-drive}/Users/oqyude/Storage";
+      target = "Storage";
+    };
   };
 }
