b3cf77203c66f8f8e5284f88fd05a84ff682e42a
oqyude <oqyude@gmail.com>
2025-09-16 18:53:09 +0300

    123

--- homeConfigurations/modules/external.nix
+++ homeConfigurations/modules/external.nix
@@ -11,5 +11,17 @@
       source = config.lib.file.mkOutOfStoreSymlink "${xlib.dirs.vetymae-drive}/Users/oqyude/Storage";
       target = "Storage";
     };
+    "Music" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${xlib.dirs.vetymae-drive}/Users/oqyude/Music";
+      target = "Music";
+    };
+    "Misc" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${xlib.dirs.vetymae-drive}/Users/oqyude/Misc";
+      target = "Misc";
+    };
+    "Vaults" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${xlib.dirs.vetymae-drive}/Users/oqyude/Vaults";
+      target = "Vaults";
+    };
   };
 }
