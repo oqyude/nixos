38e875d9b19d58f9e5b737c19ff62e1e30be9263
oqyude <oqyude@gmail.com>
2025-09-16 20:27:44 +0300

    123

--- nixosModules/services/zapret.nix
+++ nixosModules/services/zapret.nix
@@ -5,37 +5,48 @@
   ...
 }:
 {
-  systemd.services.zapret = {
+  services.zapret = {
     enable = true;
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    description = "Custom Script Service";
-    path = with pkgs; [
-      sudo
-      git
-      nftables
-      iproute2
-      coreutils
+    params = [
+      "--dpi-desync=fake,disorder2"
+      "--dpi-desync-ttl=1"
+      "--dpi-desync-autottl=2"
     ];
-    unitConfig = {
-      After = [ "network-online.target" ];
-      Wants = [ "network-online.target" ];
-    };
-    serviceConfig = {
-      Type = "simple";
-      WorkingDirectory = "${xlib.dirs.user-services}/zapret";
-      User = "root";
-      ExecStart = "${pkgs.bash}/bin/bash ${xlib.dirs.user-services}/zapret/main_script.sh -nointeractive";
-      ExecStop = "${pkgs.bash}/bin/bash ${xlib.dirs.user-services}/zapret/stop_and_clean_nft.sh";
-      ExecStopPost = ''
-        ${pkgs.coreutils}/bin/echo  "Сервис завершён"
-      '';
-      PIDFile = ''
-        /run/zapret_discord_youtube.pid
-      '';
-      Restart = "on-failure";
-      RestartSec = "5s";
-    };
   };
-
+  # systemd.services.zapret = {
+  #   enable = true;
+  #   after = [ "network.target" ];
+  #   wantedBy = [ "multi-user.target" ];
+  #   description = "Custom Script Service";
+  #   path = with pkgs; [
+  #     sudo
+  #     git
+  #     nftables
+  #     iproute2
+  #     coreutils
+  #   ];
+  #   unitConfig = {
+  #     After = [ "network-online.target" ];
+  #     Wants = [ "network-online.target" ];
+  #   };
+  #   serviceConfig = {
+  #     Type = "simple";
+  #     WorkingDirectory = "${xlib.dirs.user-services}/zapret";
+  #     User = "root";
+  #     ExecStart = ''
+  #       /run/current-system/sw/bin/bash ${xlib.dirs.user-services}/zapret/main_script.sh -nointeractive
+  #     '';
+  #     ExecStop = ''
+  #       /run/current-system/sw/bin/bash ${xlib.dirs.user-services}/zapret/stop_and_clean_nft.sh
+  #     '';
+  #     # ExecStopPost = ''
+  #     #   /run/current-system/sw/bin/echo  "Сервис завершён"
+  #     # '';
+  #     PIDFile = ''
+  #       /run/zapret_discord_youtube.pid
+  #     '';
+  #     # Restart = "on-failure";
+  #     # RestartSec = "5s";
+  #   };
+  # };
 }
