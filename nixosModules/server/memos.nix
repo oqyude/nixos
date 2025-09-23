7e974e2f12471c424ba1859466959f9d6c1411e5
oqyude <oqyude@gmail.com>
2025-09-23 19:44:21 +0300

    123

--- nixosModules/server/memos.nix
+++ nixosModules/server/memos.nix
@@ -8,12 +8,16 @@
     enable = true;
     openFirewall = true;
     settings = {
-        #MEMOS_MODE = "prod";
+        MEMOS_MODE = "prod";
         MEMOS_ADDR = "0.0.0.0";
         MEMOS_PORT = "5230";
-        #MEMOS_DATA = config.services.memos.dataDir;
+        MEMOS_DATA = config.services.memos.dataDir;
         MEMOS_DRIVER = "sqlite";
         MEMOS_INSTANCE_URL = "http://0.0.0.0:5230";
     };
+    user = "${xlib.device.username}";
+    group = "users";
+    dataDir = "${xlib.dirs.server-home}/Services/immich";
   };
+  
 }
