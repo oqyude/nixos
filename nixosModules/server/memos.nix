292a371ee06a1dfe5c589f9c939c2450331b0fb2
oqyude <oqyude@gmail.com>
2025-09-23 19:27:03 +0300

    132

--- nixosModules/server/memos.nix
+++ nixosModules/server/memos.nix
@@ -9,11 +9,11 @@
     openFirewall = true;
     settings = {
         #MEMOS_MODE = "prod";
-        MEMOS_ADDR = "127.0.0.1";
+        MEMOS_ADDR = "0.0.0.0";
         MEMOS_PORT = "5230";
         #MEMOS_DATA = config.services.memos.dataDir;
         MEMOS_DRIVER = "sqlite";
-        MEMOS_INSTANCE_URL = "http://localhost:5230";
+        MEMOS_INSTANCE_URL = "http://0.0.0.0:5230";
     };
   };
 }
