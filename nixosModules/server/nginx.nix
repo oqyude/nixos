518e7392352992ef69c70307ca437f60ea994ec1
oqyude <oqyude@gmail.com>
2025-09-23 19:31:52 +0300

    123

--- nixosModules/server/nginx.nix
+++ nixosModules/server/nginx.nix
@@ -41,20 +41,20 @@
         #     }
         #   ];
         # };
-        "localhost:9980" = {
-          forceSSL = false;
-          enableACME = false;
-          listen = [
-            {
-              addr = "100.64.0.0";
-              port = 9980;
-            }
-            {
-              addr = "192.168.1.20";
-              port = 9980;
-            }
-          ];
-        };
+        # "localhost:9980" = {
+        #   forceSSL = false;
+        #   enableACME = false;
+        #   listen = [
+        #     {
+        #       addr = "100.64.0.0";
+        #       port = 9980;
+        #     }
+        #     {
+        #       addr = "192.168.1.20";
+        #       port = 9980;
+        #     }
+        #   ];
+        # };
         # "localhost:5230" = {
         #   forceSSL = false;
         #   enableACME = false;
