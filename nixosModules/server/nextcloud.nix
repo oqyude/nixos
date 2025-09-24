a3f6f9ad6691b628e203fefb49b9ec782382341a
oqyude <oqyude@gmail.com>
2025-09-24 23:25:15 +0300

    123

--- nixosModules/server/nextcloud.nix
+++ nixosModules/server/nextcloud.nix
@@ -21,7 +21,7 @@ in
       };
       secrets = [ "${inputs.zeroq-credentials}/services/nextcloud/jwt-secret.txt" ];
     };
-    nextcloud = { # в рот манал
+    nextcloud = {
       enable = true;
       package = pkgs.nextcloud31;
       hostName = "nextcloud.local";
