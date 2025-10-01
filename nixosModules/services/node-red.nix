a99747dfb29e248c7ff2b368f0dd51aa245a6c23
oqyude <oqyude@gmail.com>
2025-10-01 12:59:18 +0300

    123

--- nixosModules/services/node-red.nix
+++ nixosModules/services/node-red.nix
@@ -14,7 +14,7 @@
     userDir = "${xlib.dirs.services-mnt-folder}/node-red";
     configFile = "${inputs.zeroq-credentials}/configs/node-red/settings.js";
   };
-  
+
   systemd.tmpfiles.rules = [
     "z ${xlib.dirs.services-mnt-folder}/node-red 0750 node-red node-red -"
   ];
