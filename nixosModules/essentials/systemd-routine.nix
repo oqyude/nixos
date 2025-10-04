86e70d5fb188bf930573767f4a634a9b21df2de6
oqyude <oqyude@gmail.com>
2025-10-04 12:19:30 +0300

    123

---
+++ nixosModules/essentials/systemd-routine.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  xlib,
+  ...
+}:
+{
+  systemd = {
+    services.nixos-auto-rebuild = {
+        description = "Auto rebuild NixOS config";
+        serviceConfig = {
+        Type = "oneshot";
+        User = "${xlib.devices.admin}";
+        WorkingDirectory = "/etc/nixos";
+        ExecStart = "gp-ns";
+        };
+    };
+    timers.nixos-auto-rebuild = {
+        description = "Run NixOS auto rebuild at 4am daily";
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+            OnCalendar = "*-*-* 04:00:00";
+            Persistent = true;
+        };
+    };
+  };
+}
+
