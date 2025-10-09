2e0e169295d4d0524e554024c9496fcb3ecab751
oqyude <oqyude@gmail.com>
2025-10-10 00:55:59 +0300

    123

--- nixosModules/server/miniflux.nix
+++ nixosModules/server/miniflux.nix
@@ -12,6 +12,15 @@
       CLEANUP_FREQUENCY = 48;
       LISTEN_ADDR = "0.0.0.0:6061";
     };
-    adminCredentialsFile = "${inputs.zeroq-credentials}/services/miniflux/admin-pass.txt";
+    # adminCredentialsFile = "${inputs.zeroq-credentials}/services/miniflux/admin-pass.txt";
+    adminCredentialsFile = config.sops.secrets.minifluxenv.path;
+  };
+
+  sops.secrets.minifluxenv = {
+    sopsFile = ./secrets/miniflux.env;
+    key = null;
+    owner = "miniflux"; 
+    group = "miniflux";
+    mode = "0600";
   };
 }
