29bebbce06332bb44d4adf9f63404042cb7e841b
oqyude <oqyude@gmail.com>
2025-10-10 20:53:03 +0300

    123

--- modules/users.nix
+++ modules/users.nix
@@ -88,7 +88,7 @@
     after = [ "network-online.target" ];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = [ "${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --no-reexec" ];
+      ExecStart = [ "${pkgs.nixos-rebuild}/bin/nixos-rebuild switch" ];
     };
   };
 
