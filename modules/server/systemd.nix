791ce986348a52d04786c7943b6bcf7fa1bf5e05
oqyude <oqyude@gmail.com>
2025-10-21 11:36:30 +0300

    archivesta fix

--- modules/server/systemd.nix
+++ modules/server/systemd.nix
@@ -7,16 +7,13 @@
 }:
 {
   systemd = {
-    services.rsync-archive = {
+    services.rsync-archive = { # Archivesta
       # Make copy of files
       description = "Backup data using rsync";
       #after = [ ];
       requisite = [ "mnt-archive.mount" ]; # hard-code
       script = ''
-        ${pkgs.rsync}/bin/rsync -rtv --delete ${xlib.dirs.services-folder}/immich/ ${xlib.dirs.archive-drive}/Services/immich/
-        ${pkgs.rsync}/bin/rsync -rtv --delete ${xlib.dirs.services-folder}/nextcloud/ ${xlib.dirs.archive-drive}/Services/nextcloud/
-        ${pkgs.rsync}/bin/rsync -rtv --delete ${xlib.dirs.services-folder}/node-red/ ${xlib.dirs.archive-drive}/Services/node-red/
-        ${pkgs.rsync}/bin/rsync -rtv --delete ${xlib.dirs.services-folder}/uptime-kuma/ ${xlib.dirs.archive-drive}/Services/node-red/
+        ${pkgs.rsync}/bin/rsync -rtv --delete ${xlib.dirs.services-folder}/ ${xlib.dirs.archive-drive}/Services/
       '';
       serviceConfig = {
         Type = "oneshot";
