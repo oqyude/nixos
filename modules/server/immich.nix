9589d3dd6c28cff9712e51acfd7cbd69b859e625
oqyude <oqyude@gmail.com>
2025-10-26 09:47:25 +0300

    123

--- modules/server/immich.nix
+++ modules/server/immich.nix
@@ -27,7 +27,7 @@ in
         enableVectorChord = true;
       };
     };
-    postgresql.package = pkgs.postgresql_17;
+    # postgresql.package = pkgs.postgresql_17;
   };
 
   fileSystems."${config.services.immich.mediaLocation}" = {
