ffdb2b6f95fdfb37bb0e35bf9ec915e8dae61451
oqyude <oqyude@gmail.com>
2025-10-26 08:25:06 +0300

    123

--- modules/server/immich.nix
+++ modules/server/immich.nix
@@ -15,7 +15,7 @@ in
   services = {
     immich = {
       enable = true;
-      package = master.immich;
+      package = pkgs.immich;
       port = 2283;
       host = "0.0.0.0";
       openFirewall = true;
