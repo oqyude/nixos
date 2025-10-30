94482498977e26819dccb1328831a1579c2b4374
oqyude <oqyude@gmail.com>
2025-10-30 20:38:06 +0300

    123

--- modules/essentials/settings.nix
+++ modules/essentials/settings.nix
@@ -12,7 +12,7 @@
     settings = {
       require-sigs = false;
       substituters = [
-        "https://cache.nixos.org"
+        # "https://cache.nixos.org"
         "https://nix-community.cachix.org"
         "https://nixos-cache-proxy.cofob.dev" # https://gist.github.com/cofob/9b1fd205e6d961a45c225ae9f0af1394
         #"https://nixos.snix.store" # https://nixos.snix.store/
