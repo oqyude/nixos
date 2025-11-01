565f92a4d8e8d665c71dee250a26ab08bffbfccf
oqyude <oqyude@gmail.com>
2025-11-01 11:54:16 +0300

    123

--- modules/server/immich.nix
+++ modules/server/immich.nix
@@ -6,16 +6,16 @@
   xlib,
   ...
 }:
-# let
-#   master = import inputs.nixpkgs-master {
-#     system = "x86_64-linux";
-#   };
-# in
+let
+  master = import inputs.nixpkgs-master {
+    system = "x86_64-linux";
+  };
+in
 {
   services = {
     immich = {
       enable = true;
-      # package = pkgs.immich;
+      package = master.immich;
       port = 2283;
       host = "0.0.0.0";
       openFirewall = true;
