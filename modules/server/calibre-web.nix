c99f41500699e4c9f42da963813fe31fad5e2760
oqyude <oqyude@gmail.com>
2025-11-02 12:30:05 +0300

    123

--- modules/server/calibre-web.nix
+++ modules/server/calibre-web.nix
@@ -5,10 +5,14 @@
   ...
 }:
 let
-    work = import (builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz";
-    }) {};
-    # myPkg = work.calibre-web;
+  work = import (builtins.fetchGit {
+      # Descriptive name to make the store path easier to identify
+      name = "my-old-revision";
+      url = "https://github.com/NixOS/nixpkgs/";
+      ref = "refs/heads/nixpkgs-unstable";
+      rev = "e6f23dc08d3624daab7094b701aa3954923c6bbb";
+  }) {};
+  # myPkg = work.calibre-web;
 in
 {
   # services.calibre-server.package = stable.calibre;
