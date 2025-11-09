f9c50b2d602330a8e251974cc17e525c63e364b6
oqyude <oqyude@gmail.com>
2025-11-09 12:08:09 +0300

    123

--- modules/essentials/tty.nix
+++ modules/essentials/tty.nix
@@ -19,9 +19,9 @@
       theme = "robbyrussell";
     };
     shellInit = ''
-beetn() {
-  echo "$*" | aichat -cer beets
-}
+      beetn() {
+        echo "$*" | aichat -cer beets
+      }
     '';
     shellAliases = {
       # shell
