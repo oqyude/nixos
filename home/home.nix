c09485e2be29bb55a861fe79685a2d28029ebe43
oqyude <oqyude@gmail.com>
2025-12-30 16:04:23 +0300

    123

--- home/home.nix
+++ home/home.nix
@@ -1,10 +1,15 @@
 { inputs, ... }@flakeContext:
 let
   nixosModule =
-    { xlib, ... }:
+    {
+      config,
+      lib,
+      xlib,
+      ...
+    }:
     let
-
-      homeModule =
+      mkHomeModule =
+        { username }:
         {
           config,
           lib,
@@ -16,31 +21,54 @@ let
             (./. + "/${xlib.device.type}.nix")
           ];
           home = {
-            username = xlib.device.username;
+            username = username;
             stateVersion = lib.mkDefault "25.05";
-            homeDirectory = "/home/${config.home.username}";
+            homeDirectory = if username == "root" then "/${username}" else "/home/${username}";
             enableNixpkgsReleaseCheck = false;
           };
         };
 
-      rootModule =
-        {
-          config,
-          lib,
-          pkgs,
-          ...
-        }:
-        {
-          imports = [
-            (./. + "/${xlib.device.type}.nix")
-          ];
-          home = {
-            username = lib.mkDefault "root";
-            stateVersion = lib.mkDefault "25.05";
-            homeDirectory = lib.mkDefault "/${config.home.username}";
-            enableNixpkgsReleaseCheck = false;
-          };
-        };
+      mkModule = username: mkHomeModule { username = username; };
+
+      homeModule = mkModule xlib.device.username;
+      rootModule = mkModule "root";
+      # homeModule =
+      #   {
+      #     config,
+      #     lib,
+      #     pkgs,
+      #     ...
+      #   }:
+      #   {
+      #     imports = [
+      #       (./. + "/${xlib.device.type}.nix")
+      #     ];
+      #     home = {
+      #       username = xlib.device.username;
+      #       stateVersion = lib.mkDefault "25.05";
+      #       homeDirectory = "/home/${config.home.username}";
+      #       enableNixpkgsReleaseCheck = false;
+      #     };
+      #   };
+
+      # rootModule =
+      #   {
+      #     config,
+      #     lib,
+      #     pkgs,
+      #     ...
+      #   }:
+      #   {
+      #     imports = [
+      #       (./. + "/${xlib.device.type}.nix")
+      #     ];
+      #     home = {
+      #       username = "root";
+      #       stateVersion = lib.mkDefault "25.05";
+      #       homeDirectory = lib.mkDefault "/${config.home.username}";
+      #       enableNixpkgsReleaseCheck = false;
+      #     };
+      #   };
 
     in
     {
