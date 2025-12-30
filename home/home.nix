5d8b0f0670c108d614d80cf9a0d2dc937bd4159e
oqyude <oqyude@gmail.com>
2025-12-30 16:12:34 +0300

    123

--- home/home.nix
+++ home/home.nix
@@ -4,80 +4,30 @@ let
     {
       config,
       lib,
+      pkgs,
       xlib,
       ...
     }:
     let
-      mkHomeModule =
-        { username }:
-        {
-          config,
-          lib,
-          pkgs,
-          ...
-        }:
-        {
-          imports = [
-            (./. + "/${xlib.device.type}.nix")
-          ];
-          home = {
-            username = username;
-            stateVersion = lib.mkDefault "25.05";
-            homeDirectory = if username == "root" then "/${username}" else "/home/${username}";
-            enableNixpkgsReleaseCheck = false;
-          };
+      mkHomeModule = username: {
+        imports = [
+          (./. + "/${xlib.device.type}.nix")
+        ];
+        home = {
+          username = username;
+          stateVersion = lib.mkDefault "25.05";
+          homeDirectory = if username == "root" then "/${username}" else "/home/${username}";
+          enableNixpkgsReleaseCheck = false;
         };
-
-      mkModule = username: mkHomeModule { username = username; };
-
-      homeModule = mkModule xlib.device.username;
-      rootModule = mkModule "root";
-      # homeModule =
-      #   {
-      #     config,
-      #     lib,
-      #     pkgs,
-      #     ...
-      #   }:
-      #   {
-      #     imports = [
-      #       (./. + "/${xlib.device.type}.nix")
-      #     ];
-      #     home = {
-      #       username = xlib.device.username;
-      #       stateVersion = lib.mkDefault "25.05";
-      #       homeDirectory = "/home/${config.home.username}";
-      #       enableNixpkgsReleaseCheck = false;
-      #     };
-      #   };
-
-      # rootModule =
-      #   {
-      #     config,
-      #     lib,
-      #     pkgs,
-      #     ...
-      #   }:
-      #   {
-      #     imports = [
-      #       (./. + "/${xlib.device.type}.nix")
-      #     ];
-      #     home = {
-      #       username = "root";
-      #       stateVersion = lib.mkDefault "25.05";
-      #       homeDirectory = lib.mkDefault "/${config.home.username}";
-      #       enableNixpkgsReleaseCheck = false;
-      #     };
-      #   };
-
+      };
     in
     {
       home-manager = {
         useGlobalPkgs = true;
         useUserPackages = true;
         users = {
-          "${xlib.device.username}" = homeModule;
-          root = rootModule;
+          "${xlib.device.username}" = mkHomeModule xlib.device.username;
+          root = mkHomeModule "root";
         };
         sharedModules = [
           inputs.plasma-manager.homeModules.plasma-manager
