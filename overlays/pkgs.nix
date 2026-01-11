c402cfa7cf5a19ae846779858fa4ecd2619c5945
oqyude <oqyude@gmail.com>
2026-01-11 13:21:13 +0300

    123

--- overlays/pkgs.nix
+++ overlays/pkgs.nix
@@ -1,11 +1,3 @@
 { inputs, ... }:
-let
-  configHome = "123";
-in
 self: super: {
-  yazi = super.yazi.overrideDerivation (old: {
-    passthru = (old.passthru or { }) // {
-      configHome = configHome;
-    };
-  });
 }
