df4c7e1012a30da8421f77d1499155a8c601f66a
oqyude <oqyude@gmail.com>
2026-01-15 17:00:32 +0300

    123

--- configurations/disko/mini-laptop.nix
+++ configurations/disko/mini-laptop.nix
@@ -3,40 +3,20 @@
   disko.devices = {
     disk = {
       "${xlib.device.hostname}" = {
-        device = "/dev/sda";
+        device = "/dev/nvme0n1p4";
         type = "disk";
         content = {
           type = "gpt";
           partitions = {
-            boot = {
-              type = "EF02";
-              size = "1M";
-            };
-            ESP = {
-              size = "500M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
-            };
             root = {
               size = "100%";
               content = {
                 type = "filesystem";
                 format = "ext4";
                 mountpoint = "/";
-                # extraArgs = [
-                #   "-L ${xlib.device.hostname}" # Filesystem label
-                # ];
-              };
-            };
-            swap = {
-              size = "2048M";
-              content = {
-                type = "swap";
+                extraArgs = [
+                  "-L ${xlib.device.hostname}" # Filesystem label
+                ];
               };
             };
           };
---
+++ configurations/hardware/mini-laptop.nix
@@ -0,0 +1,63 @@
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  xlib,
+  ...
+}:
+
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot = {
+    initrd.availableKernelModules = [
+      "nvme"
+      "xhci_pci"
+      "thunderbolt"
+      "usb_storage"
+      "uas"
+      "usbhid"
+      "sd_mod"
+    ];
+    kernelModules = [
+      "kvm-amd"
+      "amdgpu"
+    ];
+    extraModulePackages = [ ];
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-partlabel/disk-${xlib.device.hostname}-root";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/E4BB-6C2F";
+      # device = "/dev/disk/by-partlabel/disk-${xlib.device.hostname}-ESP";
+      fsType = "vfat";
+      options = [
+        "fmask=0077"
+        "dmask=0077"
+      ];
+    };
+  };
+
+  # swapDevices = [
+  #   { device = "/dev/disk/by-partlabel/disk-${xlib.device.hostname}-swap"; }
+  # ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp100s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp98s0.useDHCP = lib.mkDefault true;
+
+  #nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
