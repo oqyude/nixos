8177d1e4c2cfa75b7dd3234f02428470a76b2aa4
oqyude <oqyude@gmail.com>
2026-01-17 13:48:21 +0300

    123

--- configurations/default.nix
+++ configurations/default.nix
@@ -2,7 +2,7 @@
 {
   nixosConfigurations = {
     atoridu = import ./mini-pc.nix flakeContext; # atoridu
-    lamet = import ./mini-laptop.nix flakeContext; # lamet
+    rydiwo = import ./mini-laptop.nix flakeContext; # rydiwo
     otreca = import ./vds.nix flakeContext; # vds
     sapphira = import ./server.nix flakeContext; # sapphira
     wsl = import ./wsl.nix flakeContext; # wsl
