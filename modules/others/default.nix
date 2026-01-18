7b91a33b43cdb89c4c4968f4a060f4f47fa497f8
oqyude <oqyude@gmail.com>
2026-01-18 17:51:39 +0300

    123

--- modules/others/default.nix
+++ modules/others/default.nix
@@ -15,7 +15,7 @@ in
         isNormalUser = true;
         group = "users";
         description = "Snity";
-        hashedPasswordFile = "$y$j9T$851xwObfIp7SYzIyFtH.k1$mNofT2sxEAV50Kxgmwvqc6Kj/3B/fJoPP8qgn./siEB"; # hashed_password
+        hashedPassword = "$y$j9T$851xwObfIp7SYzIyFtH.k1$mNofT2sxEAV50Kxgmwvqc6Kj/3B/fJoPP8qgn./siEB"; # hashed_password
         homeMode = "700";
         home = "/home/${user}";
         extraGroups = [
