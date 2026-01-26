4245b45a48142efd6d84516b3d48de7663be7720
oqyude <oqyude@gmail.com>
2026-01-26 18:22:55 +0300

    123

--- modules/vds/nginx.nix
+++ modules/vds/nginx.nix
@@ -72,12 +72,6 @@ in
             "/guest/" = {
               proxyPass = "http://${server}:80";
               proxyWebsockets = true;
-              extraConfig = ''
-                proxy_set_header Host $host;
-                proxy_set_header X-Real-IP $remote_addr;
-                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                proxy_set_header X-Forwarded-Proto $scheme;
-              '';
             };
             # "/.well-known/discord" = {
             #   extraConfig = ''
