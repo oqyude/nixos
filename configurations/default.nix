63dd5218033cac32b61a5b31b366f0df44d7dad7
oqyude <oqyude@gmail.com>
2026-03-01 16:40:07 +0300

    123

--- configurations/default.nix
+++ configurations/default.nix
@@ -5,7 +5,7 @@
     atoridu = import ./mini-pc.nix flakeContext; # atoridu
     rydiwo = import ./mini-laptop.nix flakeContext; # rydiwo
     otreca = import ./vds.nix flakeContext; # vds
-    otreca-new = import ./vds.nix flakeContext; # vds-new
+    otreca-new = import ./vds-new.nix flakeContext; # vds-new
     sapphira = import ./server.nix flakeContext; # sapphira
     wsl = import ./wsl.nix flakeContext; # wsl
   };
