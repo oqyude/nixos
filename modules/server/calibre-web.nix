d6e2e73cc89446d18815e4a4964227fe3595dffd
oqyude <oqyude@gmail.com>
2026-03-03 23:54:21 +0300

    1

--- modules/server/calibre-web.nix
+++ modules/server/calibre-web.nix
@@ -11,7 +11,7 @@ let
 in
 {
   services.calibre-web = {
-    package = stable.calibre-web;
+    # package = stable.calibre-web;
     enable = true;
     group = "users";
     user = "${xlib.device.username}";
