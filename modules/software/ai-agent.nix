f61ecec9b30b490b197dfec4b80d7c4caa6e66e1
oqyude <oqyude@gmail.com>
2026-03-06 19:40:44 +0300

    12

--- modules/software/ai-agent.nix
+++ modules/software/ai-agent.nix
@@ -1,12 +1,18 @@
 {
   config,
+  inputs,
   lib,
   pkgs,
   xlib,
   ...
 }:
+let
+  master = import inputs.nixpkgs-master {
+    system = "x86_64-linux";
+  };
+in
 {
   environment.systemPackages = [
-    pkgs.open-interpreter
+    master.open-interpreter
   ];
 }
