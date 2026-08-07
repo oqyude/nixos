682ab4aa01e5f56bd4b1d9f58a4b9921dbf00ef0
oqyude <oqyude@gmail.com>
2026-08-07 20:35:52 +0300

    path moving

--- modules/server/syncthing.nix
+++ modules/server/syncthing.nix
@@ -15,7 +15,7 @@ in
     # package = master.syncthing;
     systemService = true;
     guiAddress = "0.0.0.0:8384";
-    configDir = "${xlib.dirs.storage}/Syncthing/${xlib.device.hostname}";
+    configDir = "${xlib.dirs.storage}/persist/Syncthing/${xlib.device.hostname}";
     dataDir = "${xlib.dirs.server-home}";
     group = "users";
     user = "${xlib.device.username}";
