b933436a6e6e2fc86d8decf360f28e020c23db7e
oqyude <oqyude@gmail.com>
2026-08-11 23:36:28 +0300

    minecraft: use linkFarmFromDrvs for mods

--- modules/server/minecraft.nix
+++ modules/server/minecraft.nix
@@ -39,20 +39,25 @@ in
           enable-rcon = true;
           "rcon.password" = "zeroq";
         };
-        symlinks.mods = {
-          "lithium-fabric-0.25.3+mc26.2.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/f7vZ0VWU/lithium-fabric-0.25.3%2Bmc26.2.jar";
-            hash = "sha256-/d6S4jjoB1+JrX9wHyo9WFSviLqaZ2VxhKRAexBKxWM=";
-          };
-          "ferritecore-9.0.0-fabric.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/uXXizFIs/versions/d5ddUdiB/ferritecore-9.0.0-fabric.jar";
-            hash = "sha256-ITlmxy7ZZ6zHOSvrKKhm+6MB/1a5l2wueAHC233mvyI=";
-          };
-          "krypton-0.3.1.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/fQEb0iXm/versions/5WeL0Nkz/krypton-0.3.1.jar";
-            hash = "sha256-XqiQFWGXPSnlHnUUadUtkhAPNIq0YeEYb2cBLpNCDEg=";
-          };
-        };
+        symlinks.mods = pkgs.linkFarmFromDrvs "mods" (
+          builtins.attrValues {
+            Lithium = pkgs.fetchurl {
+              name = "lithium-fabric-0.25.3+mc26.2.jar";
+              url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/f7vZ0VWU/lithium-fabric-0.25.3%2Bmc26.2.jar";
+              hash = "sha256-/d6S4jjoB1+JrX9wHyo9WFSviLqaZ2VxhKRAexBKxWM=";
+            };
+            FerriteCore = pkgs.fetchurl {
+              name = "ferritecore-9.0.0-fabric.jar";
+              url = "https://cdn.modrinth.com/data/uXXizFIs/versions/d5ddUdiB/ferritecore-9.0.0-fabric.jar";
+              hash = "sha256-ITlmxy7ZZ6zHOSvrKKhm+6MB/1a5l2wueAHC233mvyI=";
+            };
+            Krypton = pkgs.fetchurl {
+              name = "krypton-0.3.1.jar";
+              url = "https://cdn.modrinth.com/data/fQEb0iXm/versions/5WeL0Nkz/krypton-0.3.1.jar";
+              hash = "sha256-XqiQFWGXPSnlHnUUadUtkhAPNIq0YeEYb2cBLpNCDEg=";
+            };
+          }
+        );
       };
     };
   };
