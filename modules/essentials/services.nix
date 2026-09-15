309644eab2af43c3b4109055a6e89beaaafb9ba7
oqyude <oqyude@gmail.com>
2026-09-15 21:22:33 +0300

    fixes

--- modules/essentials/services.nix
+++ modules/essentials/services.nix
@@ -8,8 +8,10 @@
 
   # All real hosts (not the bare "minimal" test config) get OOM protection
   # and a bounded journal.
-  services.earlyoom.enable = lib.mkIf (xlib.device.type != "minimal") true;
-  services.journald.extraConfig = lib.mkIf (xlib.device.type != "minimal") ''
-    SystemMaxUse=512M
-  '';
+  services = {
+    earlyoom.enable = lib.mkIf (xlib.device.type != "minimal") true;
+    journald.settings.Journal = lib.mkIf (xlib.device.type != "minimal") {
+      SystemMaxUse = "512M";
+    };
+  };
 }
