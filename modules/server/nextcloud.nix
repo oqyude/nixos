bc3566d80eb8f379b017f46191d2b9d87d7e57dd
oqyude <oqyude@gmail.com>
2026-09-23 22:14:01 +0300

    nextcloud35

--- modules/server/nextcloud.nix
+++ modules/server/nextcloud.nix
@@ -49,7 +49,7 @@
     };
     nextcloud = {
       enable = true;
-      package = pkgs.nextcloud34;
+      package = pkgs.nextcloud35;
       hostName = "nextcloud.private";
       database.createLocally = true;
       home = "${xlib.dirs.services-mnt-folder}/nextcloud";
